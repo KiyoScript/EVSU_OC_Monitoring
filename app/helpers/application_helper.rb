3409385ed34ca2f62db5cf53031780f7e45e54cc
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-06-05 22:36:22 +0800

    modified department and student display table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,17 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  GENDER_BADGE_CLASS = {
+    "female" => "badge text-bg-warning",
+    "male" => "badge text-bg-info"
+  }.freeze
+
+
+  def gender_badge(gender)
+    badge_class = GENDER_BADGE_CLASS[gender] || GENDER_BADGE_CLASS["female"]
+    content_tag(:span, gender.capitalize, class: badge_class)
+  end
+
   def notification_container_class
     if user_signed_in?
       base_class = "transition transform duration-5000 hidden toast-container position-fixed top-0 end-0 px-3 pb-3 pt-4 mt-5"
