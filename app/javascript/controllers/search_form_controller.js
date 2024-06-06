f548affc453835ce61903638eecd3df51632a5cb
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-06-07 06:57:11 +0800

    search form autofocus

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -3,12 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search-form"
 export default class extends Controller {
   connect() {
-    this.element.addEventListener("input", this.handleInput.bind(this));
+    this.element.addEventListener("click", this.autoFocus.bind(this));
     this.element.addEventListener("submit", this.handleSubmit.bind(this));
   }
 
-  handleInput() {
-    this.element.submit();
+  autoFocus(event) {
+    const input = this.element.querySelector('input');
+    if (input) {
+      input.focus();
+    }
   }
 
   handleSubmit(event) {
