07a195389b2da93c34164d3de95a70bb614ea233
nathaniel <nathanielkatesimon@gmail.com>
2024-06-08 08:26:47 +0800

    Update configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:                      'smtp.gmail.com',
     port:                         587,
-    domain:                       Rails.application.credentials.gmail.domain,
-    user_name:                    Rails.application.credentials.gmail.user_name,
-    password:                     Rails.application.credentials.gmail.password,
+    domain:                       Rails.application.credentials.dig(:gmail, :domain),
+    user_name:                    Rails.application.credentials.dig(:gmail, :user_name),
+    password:                     Rails.application.credentials.dig(:gmai, :password),
     authentication:               'plain',
     enable_starttls_auto:         true
   }
